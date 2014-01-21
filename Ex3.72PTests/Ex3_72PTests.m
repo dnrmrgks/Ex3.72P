fabd21faaa2e14083a50baf510731cd8cdadf4f0
park sung chan <dnrmrgks@naver.com>
2014-01-21 11:19:18 +0900

    Initial Commit

---
+++ Ex3.72PTests/Ex3_72PTests.m
@@ -0,0 +1,34 @@
+//
+//  Ex3_72PTests.m
+//  Ex3.72PTests
+//
+//  Created by SDT-1 on 2014. 1. 21..
+//  Copyright (c) 2014년 T. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Ex3_72PTests : XCTestCase
+
+@end
+
+@implementation Ex3_72PTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
